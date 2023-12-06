ddb258bd59d72c4fd1853e843431f02a31f58b46
AndreasOun <andreasoun29@gmail.com>
2023-12-06 12:34:11 +0200

    Added title

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
+import Title from './components/Title';
 import './App.css';
 
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <Title />
       <AboutMe />
     </div>
   );
---
+++ portfolio/src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.css';
+
+const Title = () => {
+  return (
+    <div className="title-container">
+      <h1 className="title">Hello, I'm a Software Developer</h1>
+    </div>
+  );
+};
+
+export default Title;
